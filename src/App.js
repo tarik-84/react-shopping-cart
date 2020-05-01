25bd23fd8634d57c380bb7ed1db8acf567464c6e
tarik-84 <niceloveman200@yahoo.com>
2020-05-01 16:31:50 -0400

    first push

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+//contexts
+import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item])
 	};
 
 	return (
@@ -20,14 +25,19 @@ function App() {
 			<Navigation cart={cart} />
 
 			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+			<ProductContext.Provider value={{products, addItem}}>
+			  <CartContext.Provider value={cart}> 	
+				<Route exact path="/">
+					<Products />
+				</Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+				<Route path="/cart">
+					<ShoppingCart />
+				</Route>
+			  </CartContext.Provider> 
+			</ProductContext.Provider>
 		</div>
+	  	
 	);
 }
 
